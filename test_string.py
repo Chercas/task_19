b5961ebc066a282e6000eedf727a892a1b2e250c
Kostiantyn <chercas.kostya@gmail.com>
2018-01-11 10:21:56 +0200

    Made by [:2] method, looks fine

--- test_string.py
+++ test_string.py
@@ -1,6 +1,10 @@
 string = input('Enter string, please')
-first_2 = string[0]+string[1]
-if first_2 == 'Is':
-    print('The string is {}'.format(string))
+# first_2 = string[0]+string[1]
+# if first_2 == 'Is':
+#     print('The string is {}'.format(string))
+# else:
+#     print('The string is {}'.format('Is' + ' ' + string))
+if string[:2] == 'Is':
+    print(string)
 else:
-    print('The string is {}'.format('Is' + ' ' + string))
+    print('Is ' + string)
